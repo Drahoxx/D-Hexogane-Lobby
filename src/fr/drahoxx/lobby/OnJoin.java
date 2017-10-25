11e6e18871a762293163417ebad2492895669ecb
Drahoxx <leo.chaigneau2@gmail.com>
2017-10-25 23:11:51 +0200

    a

--- src/fr/drahoxx/lobby/OnJoin.java
+++ src/fr/drahoxx/lobby/OnJoin.java
@@ -7,7 +7,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
